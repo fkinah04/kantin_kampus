aa36c56b82de57ef2b2a567c9c30556fd68a4849
zaininijar <zaininijar7610@gmail.com>
2024-01-10 23:35:27 +0700

    add icon and design login

--- admin/index.php
+++ admin/index.php
@@ -1,62 +1,62 @@
 <?php
-    session_start();
+session_start();
 
-    if ($_SESSION['login']==FALSE) {
-        header('location:../login.php');
-    }
+if ($_SESSION['login'] == false) {
+    header('location:../login.php');
+}
 ?>
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Dashboard Akademik</title>
-    <link href="../css/bootstrap.min.css" rel="stylesheet" >
-
-
+    <link href="../css/bootstrap.min.css" rel="stylesheet" />
 
     <style>
-      .bd-placeholder-img {
+    .bd-placeholder-img {
         font-size: 1.125rem;
         text-anchor: middle;
         -webkit-user-select: none;
         -moz-user-select: none;
         user-select: none;
-      }
+    }
 
-      @media (min-width: 768px) {
+    @media (min-width: 768px) {
         .bd-placeholder-img-lg {
-          font-size: 3.5rem;
+            font-size: 3.5rem;
         }
-      }
+    }
 
-      .b-example-divider {
+    .b-example-divider {
         height: 3rem;
-        background-color: rgba(0, 0, 0, .1);
-        border: solid rgba(0, 0, 0, .15);
+        background-color: rgba(0, 0, 0, 0.1);
+        border: solid rgba(0, 0, 0, 0.15);
         border-width: 1px 0;
-        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
-      }
+        box-shadow: inset 0 0.5em 1.5em rgba(0, 0, 0, 0.1),
+            inset 0 0.125em 0.5em rgba(0, 0, 0, 0.15);
+    }
 
-      .b-example-vr {
+    .b-example-vr {
         flex-shrink: 0;
         width: 1.5rem;
         height: 100vh;
-      }
+    }
 
-      .bi {
-        vertical-align: -.125em;
+    .bi {
+        vertical-align: -0.125em;
         fill: currentColor;
-      }
+    }
 
-      .nav-scroller {
+    .nav-scroller {
         position: relative;
         z-index: 2;
         height: 2.75rem;
         overflow-y: hidden;
-      }
+    }
 
-      .nav-scroller .nav {
+    .nav-scroller .nav {
         display: flex;
         flex-wrap: nowrap;
         padding-bottom: 1rem;
@@ -65,110 +65,139 @@
         text-align: center;
         white-space: nowrap;
         -webkit-overflow-scrolling: touch;
-      }
-      .bg-navbar {
+    }
+
+    .bg-navbar {
         background-color: #424769;
-      }
+    }
     </style>
 
-    
     <!-- Custom styles for this template -->
-    <link href="../css/dashboard.css" rel="stylesheet">
-  </head>
-  <body>
-    
-<header class="navbar navbar-dark sticky-top bg-navbar flex-md-nowrap p-0 shadow">
-  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#">Kantin Kampus</a>
-  <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <input class="form-control form-control-dark w-100 rounded-0 border-0" type="text" placeholder="Search" aria-label="Search">
-  <div class="navbar-nav">
-    <div class="nav-item text-nowrap">
-      <a class="nav-link px-3" href="logout.php">Sign out</a>
-    </div>
-  </div>
-</header>
+    <link href="../css/dashboard.css" rel="stylesheet" />
+</head>
 
-<div class="container-fluid">
-  <div class="row">
-    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-      <div class="position-sticky pt-3 sidebar-sticky">
-        <ul class="nav flex-column">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="index.php">
-              <span data-feather="home" class="align-text-bottom"></span>
-              Dashboard
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="index.php?p=mhs">
-              <span data-feather="user" class="align-text-bottom"></span>
-              Mahasiswa
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="index.php?p=menu">
-              <span data-feather="shopping-cart" class="align-text-bottom"></span>
-              Menu
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="index.php?p=pegawai">
-              <span data-feather="list" class="align-text-bottom"></span>
-              Pegawai
-            </a>
-          </li>
-          <?php
-            if($_SESSION['level']=='admin'){
-          ?>
-          <li class="nav-item">
-            <a class="nav-link" href="index.php?p=user">
-              <span data-feather="bar-chart-2" class="align-text-bottom"></span>
-              User
-            </a>
-          </li>
-          <?php
-          }
-          ?>
-          <li class="nav-item">
-            <a class="nav-link" href="index.php?p=supplier">
-              <span data-feather="layers" class="align-text-bottom"></span>
-              Supplier
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="index.php?p=pembayaran">
-              <span data-feather="layers" class="align-text-bottom"></span>
-              Pembayaran
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+<body>
+    <header class="navbar navbar-dark sticky-top bg-navbar flex-md-nowrap p-0 shadow">
+        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#">Kantin Kampus</a>
+        <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse"
+            data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <input class="form-control form-control-dark w-100 rounded-0 border-0" type="text" placeholder="Search"
+            aria-label="Search" />
+        <div class="navbar-nav">
+            <div class="nav-item text-nowrap">
+                <a class="nav-link px-3" href="logout.php">Sign out</a>
+            </div>
+        </div>
+    </header>
 
-    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-        <?php
+    <div class="container-fluid">
+        <div class="row">
+            <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+                <div class="position-sticky pt-3 sidebar-sticky">
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="index.php">
+                                <span data-feather="home" class="align-text-bottom"></span>
+                                Dashboard
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php?p=mhs">
+                                <span data-feather="user" class="align-text-bottom"></span>
+                                Mahasiswa
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php?p=menu">
+                                <span data-feather="shopping-cart" class="align-text-bottom"></span>
+                                Menu
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php?p=pegawai">
+                                <span data-feather="list" class="align-text-bottom"></span>
+                                Pegawai
+                            </a>
+                        </li>
+                        <?php
+            if ($_SESSION['level'] == 'admin') {
+                ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php?p=user">
+                                <span data-feather="bar-chart-2" class="align-text-bottom"></span>
+                                User
+                            </a>
+                        </li>
+                        <?php
+            }
+?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php?p=supplier">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-cart3" viewBox="0 0 16 16">
+                                    <path
+                                        d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l.84 4.479 9.144-.459L13.89 4zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
+                                </svg>
+                                Supplier
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php?p=pembayaran">
+                                <span data-feather="layers" class="align-text-bottom"></span>
+                                Pembayaran
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                <?php
         include '../koneksi.php';
-        $p=isset($_GET['p']) ? $_GET['p'] : 'dashboard'; // ternary
-        if ($p=='dashboard') include 'dashboard.php';
-        if ($p=='mhs') include 'mahasiswa.php';
-        if ($p=='menu') include 'menu.php';
-        if ($p=='pegawai') include 'pegawai.php';
-        if ($p=='user') include 'user.php';
-        if ($p=='login') include 'login.php';
-        if ($p=='supplier') include 'supplier.php';
-        if ($p=='pembayaran') include 'pembayaran.php';
-        
-        ?>
+$p = isset($_GET['p']) ? $_GET['p'] : 'dashboard'; // ternary
+if ($p == 'dashboard') {
+    include 'dashboard.php';
+}
+if ($p == 'mhs') {
+    include 'mahasiswa.php';
+}
+if ($p == 'menu') {
+    include 'menu.php';
+}
+if ($p == 'pegawai') {
+    include 'pegawai.php';
+}
+if ($p == 'user') {
+    include 'user.php';
+}
+if ($p == 'login') {
+    include 'login.php';
+}
+if ($p == 'supplier') {
+    include 'supplier.php';
+}
+if ($p == 'pembayaran') {
+    include 'pembayaran.php';
+}
 
-     
-    </main>
-  </div>
-</div>
+?>
+            </main>
+        </div>
+    </div>
 
+    <script src="/docs/5.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js"
+        integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js"
+        integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous">
+    </script>
+    <script src="../../js/dashboard.js"></script>
+</body>
 
-    <script src="/docs/5.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script><script src="../../js/dashboard.js"></script>
-  </body>
 </html>
--- login.php
+++ login.php
@@ -1,54 +1,54 @@
-
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Signin</title>
     <link href="CSS/bootstrap.min.css" rel="stylesheet">
 
     <style>
-      .bd-placeholder-img {
+    .bd-placeholder-img {
         font-size: 1.125rem;
         text-anchor: middle;
         -webkit-user-select: none;
         -moz-user-select: none;
         user-select: none;
-      }
+    }
 
-      @media (min-width: 768px) {
+    @media (min-width: 768px) {
         .bd-placeholder-img-lg {
-          font-size: 3.5rem;
+            font-size: 3.5rem;
         }
-      }
+    }
 
-      .b-example-divider {
+    .b-example-divider {
         height: 3rem;
         background-color: rgba(0, 0, 0, .1);
         border: solid rgba(0, 0, 0, .15);
         border-width: 1px 0;
         box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
-      }
+    }
 
-      .b-example-vr {
+    .b-example-vr {
         flex-shrink: 0;
         width: 1.5rem;
         height: 100vh;
-      }
+    }
 
-      .bi {
+    .bi {
         vertical-align: -.125em;
         fill: currentColor;
-      }
+    }
 
-      .nav-scroller {
+    .nav-scroller {
         position: relative;
         z-index: 2;
         height: 2.75rem;
         overflow-y: hidden;
-      }
+    }
 
-      .nav-scroller .nav {
+    .nav-scroller .nav {
         display: flex;
         flex-wrap: nowrap;
         padding-bottom: 1rem;
@@ -57,65 +57,69 @@
         text-align: center;
         white-space: nowrap;
         -webkit-overflow-scrolling: touch;
-      }
+    }
     </style>
 
-    
+
     <!-- Custom styles for this template -->
     <link href="../CSS/signin.css" rel="stylesheet">
-  </head>
-  <body class="text-center">
-    
-<main class="form-signin w-100 m-auto">
-  <form method="post" action="">
-    
-    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+</head>
 
-    <div class="form-floating">
-      <input type="text" class="form-control" id="floatingInput" placeholder="username" name="username">
-      <label for="floatingInput">Username</label>
-    </div>
-    <div class="form-floating">
-      <input type="password" class="form-control" id="floatingPassword" placeholder="Password" name="password">
-      <label for="floatingPassword">Password</label>
-    </div>
+<body class="text-center">
 
-    <div class="checkbox mb-3">
-      <label>
-        <input type="checkbox" value="remember-me"> Remember me
-      </label>
-    </div>
-    <button class="w-100 btn btn-lg btn-primary" type="submit" name="submit">Sign in</button>
-    <p class="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
-  </form>
+    <main class="form-signin w-100 m-auto">
+        <form method="post" action="">
 
-  <?php
+            <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+            <div class="form-floating">
+                <input type="text" class="form-control" id="floatingInput" placeholder="username" name="username">
+                <label for="floatingInput">Username</label>
+            </div>
+            <div class="form-floating">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password"
+                    name="password">
+                <label for="floatingPassword">Password</label>
+            </div>
+
+            <div class="checkbox mb-3">
+                <label>
+                    <input type="checkbox" value="remember-me"> Remember me
+                </label>
+            </div>
+            <button class="w-100 btn btn-lg btn-primary"
+                style="background-color: #424769 !important; border: 1px solid #424769 !important" type="submit"
+                name="submit">Sign in</button>
+            <p class="mt-5 mb-3 text-muted">&copy; 2023-2024</p>
+        </form>
+
+        <?php
     if (isset($_POST['submit'])) {
         include 'koneksi.php';
-        $user=$_POST['username'];
-        $pass=$_POST['password'];
-        $cek_login=mysqli_query($db, "SELECT *FROM user WHERE username='$user' AND password='$pass'");
-        
-        $hasil_login=mysqli_num_rows($cek_login);
-        $data_user=mysqli_fetch_array($cek_login);
-        
+        $user = $_POST['username'];
+        $pass = $_POST['password'];
+        $cek_login = mysqli_query($db, "SELECT *FROM user WHERE username='$user' AND password='$pass'");
+
+        $hasil_login = mysqli_num_rows($cek_login);
+        $data_user = mysqli_fetch_array($cek_login);
+
         if($hasil_login > 0) {
             session_start();
-            $_SESSION['login']=TRUE;
-            $_SESSION['username']=$data_user['username'];
-            $_SESSION['level']=$data_user['level'];
-            $_SESSION['user_id']=$data_user['id'];
+            $_SESSION['login'] = true;
+            $_SESSION['username'] = $data_user['username'];
+            $_SESSION['level'] = $data_user['level'];
+            $_SESSION['user_id'] = $data_user['id'];
             header('location:admin/index.php');
             // echo "<script>alert('login Sukses !!')</script>";
-        }
-        else {
+        } else {
             echo "<script>alert('Login Invalid ') </script>";
         }
     }
-  ?>
-</main>
+        ?>
+    </main>
 
 
-    
-  </body>
+
+</body>
+
 </html>
